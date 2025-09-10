76e4e7a8877a1f0bcec68efa93297e6cc59cf300
yannick <yannick@accolade.coop>
2025-09-10 22:29:50 +0200

    fix

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 ```shell
 ./scripts/create-jar.sh
 ```
+
+### utilise le jar
+
+* ajouter un config.properties a côté du jar
+* initialiser l'univers : ```java -cp sheril.jar Start init```
+* initialiser l'univers : ```java -cp sheril.jar Start newRound```
--- scripts/create-jar.sh
+++ scripts/create-jar.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # a lancer à la racine
-
 export LIBS_PATH=$(find ./libs -name "*.jar" | tr '\n' ':')
 mkdir -p ./classes
 find ./sources -name "*.java" -print | xargs javac -cp "${LIBS_PATH}" -d ./classes
 jar -cvf ./sheril.jar -C ./classes .
-cd ./libs
-for jar_file in *.jar; do jar -uf ./sheril.jar "$jar_file"; done
+for jar_file in libs/*.jar; do jar -uf ./sheril.jar "$jar_file"; done
