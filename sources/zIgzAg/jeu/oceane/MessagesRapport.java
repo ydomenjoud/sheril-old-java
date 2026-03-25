de9defa167cd9a528eec7f99ab1e3eb7f44fa623
salardon <salardon@gmail.com>
2026-03-25 07:34:59 +0100

    stats: ajout des en-têtes pour le classement population VS

--- sources/zIgzAg/jeu/oceane/MessagesRapport.java
+++ sources/zIgzAg/jeu/oceane/MessagesRapport.java
@@ -159,6 +159,10 @@ public class MessagesRapport extends MessagesAbstraits{
  public static final String[] STATS_UNIVERS_RELATIONS={"Relations entres les différentes races du tour "+tour};
  public static final String[] STATS_UNIVERS_SITES={"Sites des commandants du tour "+tour,"Sites des alliances","Sites d'Océane"};
  public static final String[] STATS_RAYONNEMENT = {"Rayonnement Galactique du tour "+tour, "Propriétaire", "N°", "Race", "Système", "Influence"};
+ public static final String[] STATS_POP_VS = {
+    "Recensement des cités spatiales du tour " + tour,
+    "Place", "Nom", "N°", "Race", "Population"
+};
 
 
 
