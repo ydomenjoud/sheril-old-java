8f307170d3bbf700b32da253c255792c003b0fe0
salardon <salardon@gmail.com>
2026-05-11 22:30:53 +0200

    Update DeroulementDuTour.java avec un log general sur les degats du tour

--- sources/zIgzAg/jeu/oceane/DeroulementDuTour.java
+++ sources/zIgzAg/jeu/oceane/DeroulementDuTour.java
@@ -72,6 +72,20 @@ public class DeroulementDuTour {
 		//System.out.println("Debut des combats...");
 		Univers.notify("Debut des combats...");
 		Combat.resolutionCombats();
+
+		// --- VERIFICATION FINALE AVANT AFFICHAGE ---
+		System.out.println("---------------------------------------------------------");
+		System.out.println("[DEB-FIN] BILAN DES SCORES AVANT ECRITURE DU RAPPORT");
+		for (int i = 0; i < Univers.getCommandants().size(); i++) {
+		    Commandant c = (Commandant) Univers.getCommandants().get(i);
+		    if (c.getDegatsInfligesCeTour() > 0) {
+		        System.out.println(String.format("   > Commandant %s : %.2f points enregistrés", 
+		                           c.getNomNumero(), c.getDegatsInfligesCeTour()));
+		    }
+		}
+		System.out.println("---------------------------------------------------------");
+
+		
 		System.out.println("Gestion fin du tour...");
 		//System.out.println("Progression gouverneur");
 
