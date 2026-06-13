e8b82eeb076c4551a3fdcb805da8adf4f5fa6808
yannick <yannick@accolade.coop>
2026-06-13 17:12:25 +0200

    fix script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -cp sheril.jar Start init
-java -cp sheril.jar Start addNewGalaxy 0
 echo "0" > php/tour.txt
 echo "0" > data/commun/tour.txt
+java -cp sheril.jar Start init
+java -cp sheril.jar Start addNewGalaxy 0
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,4 +2,5 @@
 cp config.properties.sample config.properties
 cp php/secure/connect.txt.sample php/secure/connect.txt
 cp php/live/a.php.sample php/live/a.php
+mkdir -p data/commun
 chmod -R 0777 php/live/
